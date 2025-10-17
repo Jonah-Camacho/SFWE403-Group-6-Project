ea340bab4b23f153c838f846fab7bbabb0acdc9a
Ki-Yhun <jawallen@me.com>
2025-10-17 01:13:26 -0700

    Updated the CSS and JSX
    Improved the UX and UI of the page as well as made containers for the JSX more readily readable and editable.

--- ChatCat-Assistant/src/App.jsx
+++ ChatCat-Assistant/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import azLogo from "./assets/UAENGR_logo.png";
+import gearIcon from "./assets/gear-icon.svg";
 
 function App() {
   const [messages, setMessages] = useState([
@@ -26,18 +27,19 @@ function App() {
   };
 
   return (
-<>
-    <header className="header">
-      <div className="header-left">
-        <img src={azLogo} className="logo az" alt="AZ logo" />
-      </div>
-
-      <div className="header-center">
-        <h1>ChatCat Assistant</h1>
-      </div>
-    </header>
-
     <div className="app">
+      <header className="header">
+        <div className="header-left">
+          <img src={azLogo} className="logo az" alt="AZ logo"/>
+        </div>
+        <div className="header-center">
+          <h1>ChatCat Assistant</h1>
+        </div>
+        <div className="header-right">
+          <img src={gearIcon} className="logo-settings" alt="settings-logo"/>
+        </div>
+      </header>
+
       <div className="chat-container">
         <div className="chat-box">
           {messages.map((msg, index) => (
@@ -49,17 +51,16 @@ function App() {
 
         <div className="input-area">
           <input
-            type="text"
-            placeholder="Type your message..."
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            onKeyDown={(e) => e.key === "Enter" && handleSend()}
+                type="text"
+                placeholder="Type your message..."
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyDown={(e) => e.key === "Enter" && handleSend()}
           />
           <button onClick={handleSend}>Send</button>
         </div>
       </div>
     </div>
-</>
   );
 }
 
